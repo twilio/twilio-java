0d75a7bffdede2c85928a5d3864c6a00f4a7e701
Kevin Burke <kevin@twilio.com>
2013-02-14 14:15:14 -0800

    fix spec incompatibilities

--- src/main/java/com/twilio/sdk/verbs/Sip.java
+++ src/main/java/com/twilio/sdk/verbs/Sip.java
@@ -14,4 +14,30 @@ public class Sip extends Verb {
         this.allowedVerbs.add(Verb.V_URI);
         this.allowedVerbs.add(Verb.V_HEADERS);
     }
+
+    public void setUsername(String username) {
+        this.set("username", username);
+    }
+
+    public void setPassword(String password) {
+        this.set("password", password);
+    }
+
+    /**
+     * Sets the url.
+     *
+     * @param url the waiting url
+     */
+    public void setUrl(String url){
+       this.set("url", url);
+    }
+
+    /**
+     * Sets the method.
+     *
+     * @param method the new method
+     */
+    public void setMethod(String method){
+       this.set("method", method);
+    }
 }
--- src/main/java/com/twilio/sdk/verbs/Uri.java
+++ src/main/java/com/twilio/sdk/verbs/Uri.java
@@ -21,22 +21,5 @@ public class Uri extends Verb {
         this.set("password", password);
     }
 
-    /**
-     * Sets the action.
-     *
-     * @param url the new action
-     */
-    public void setAction(String url){
-       this.set("action", url);
-    }
-
-    /**
-     * Sets the method.
-     *
-     * @param method the new method
-     */
-    public void setMethod(String method){
-       this.set("method", method);
-    }
 }
 

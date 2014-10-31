72383f2778805342f7232f300516a6c9d8852f29
Sam Kimbrel <skimbrel@twilio.com>
2014-10-31 11:27:36 -0700

    Add tests for Request

--- src/test/java/com/twilio/sdk/http/RequestTest.java
+++ src/test/java/com/twilio/sdk/http/RequestTest.java
@@ -1,7 +1,10 @@
 package com.twilio.sdk.http;
 
+import com.google.common.collect.Range;
+import org.joda.time.LocalDate;
 import org.junit.Test;
 
+import static com.twilio.sdk.Assert.assertQueryStringsEqual;
 import static com.twilio.sdk.Assert.assertUrlsEqual;
 import static org.junit.Assert.*;
 
@@ -19,13 +22,81 @@ public class RequestTest {
     }
 
     @Test
-    public void testConstructURLWithParams() throws MalformedURLException {
+    public void testConstructURLWithParam() throws MalformedURLException {
         Request r = new Request(HttpMethod.GET, "http://example.com/foobar");
         r.addQueryParam("baz", "quux");
         URL url = r.constructURL();
         URL expected = new URL("http://example.com/foobar?baz=quux");
         assertUrlsEqual(expected, url);
     }
+
+    @Test
+    public void testConstructURLWithParams() throws MalformedURLException {
+        Request r = new Request(HttpMethod.GET, "http://example.com/foobar");
+        r.addQueryParam("baz", "quux");
+        r.addQueryParam("garply", "xyzzy");
+        URL url = r.constructURL();
+        URL expected = new URL("http://example.com/foobar?baz=quux&garply=xyzzy");
+        assertUrlsEqual(expected, url);
+    }
+
+    @Test
+    public void testConstructURLWithMultivaluedParam() throws MalformedURLException {
+        Request r = new Request(HttpMethod.GET, "http://example.com/foobar");
+        r.addQueryParam("baz", "quux");
+        r.addQueryParam("baz", "xyzzy");
+        URL url = r.constructURL();
+        URL expected = new URL("http://example.com/foobar?baz=quux&baz=xyzzy");
+        assertUrlsEqual(expected, url);
+    }
+
+    @Test
+    public void testConstructURLWithInequalityParam() throws MalformedURLException {
+        Request r = new Request(HttpMethod.GET, "http://example.com/foobar");
+        r.addQueryParam("baz>", "3");
+        URL url = r.constructURL();
+        URL expected = new URL("http://example.com/foobar?baz>=3");
+        assertUrlsEqual(expected, url);
+    }
+
+    @Test
+    public void testAddQueryDateRangeLowerBound() throws MalformedURLException {
+        Request r = new Request(HttpMethod.GET, "http://example.com/foobar");
+        r.addQueryDateRange("baz", Range.greaterThan(new LocalDate(2014, 1, 1)));
+        URL url = r.constructURL();
+        URL expected = new URL("http://example.com/foobar?baz>=2014-01-01");
+        assertUrlsEqual(expected, url);
+    }
+
+    @Test
+    public void testAddQueryDateRangeUpperBound() throws MalformedURLException {
+        Request r = new Request(HttpMethod.GET, "http://example.com/foobar");
+        r.addQueryDateRange("baz", Range.lessThan(new LocalDate(2014, 1, 1)));
+        URL url = r.constructURL();
+        URL expected = new URL("http://example.com/foobar?baz<=2014-01-01");
+        assertUrlsEqual(expected, url);
+    }
+
+    @Test
+    public void testAddQueryDateRangeClosed() throws MalformedURLException {
+        Request r = new Request(HttpMethod.GET, "http://example.com/foobar");
+        r.addQueryDateRange("baz", Range.closed(
+                new LocalDate(2014, 1, 1),
+                new LocalDate(2014, 6, 1)));
+        URL url = r.constructURL();
+        URL expected = new URL("http://example.com/foobar?baz>=2014-01-01&baz<=2014-06-01");
+        assertUrlsEqual(expected, url);
+    }
+
+    @Test
+    public void testEncodeFormBody() {
+        Request r = new Request(HttpMethod.POST, "http://example.com/foobar");
+        r.addPostParam("baz", "quux");
+        r.addPostParam("garply", "xyzzy");
+        String encoded = r.encodeFormBody();
+        assertQueryStringsEqual("baz=quux&garply=xyzzy", encoded);
+    }
+
 
 }
 

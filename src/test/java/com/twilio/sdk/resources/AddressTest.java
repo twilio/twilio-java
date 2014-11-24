0db18522265dabc8a323754f2a56b52ebfb3f2b7
Sam Kimbrel <skimbrel@twilio.com>
2014-11-24 14:37:46 -0800

    Update with AddressTest from generated source

--- src/test/java/com/twilio/sdk/resources/AddressTest.java
+++ src/test/java/com/twilio/sdk/resources/AddressTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertNotNull;
 
 public class AddressTest {
 
-    private static final String INSTANCE_JSON_RESPONSE = "{\"account_sid\": \"AC9fa6e6422f53463a89b5d81b2645d070\", \"city\": \"Sydney\", \"customer_name\": \"Tester\", \"date_created\": \"Mon, 06 Oct 2014 23:57:11 +0000\", \"date_updated\": \"Mon, 06 Oct 2014 23:57:11 +0000\", \"friendly_name\": \"Test\", \"iso_country\": \"AU\", \"postal_code\": \"12345\", \"region\": \"NSW\", \"sid\": \"AD79f1863d1ad0c25e795bb7577da0999a\", \"street\": \"123+Main+St\", \"uri\": \"/2010-04-01/Accounts/AC9fa6e6422f53463a89b5d81b2645d070/Addresses/AD79f1863d1ad0c25e795bb7577da0999a.json\"}";
+    private static final String INSTANCE_JSON_RESPONSE = "{\"account_sid\": \"AC9fa6e6422f53463a89b5d81b2645d070\", \"city\": \"Sydney\", \"customer_name\": \"Tester\", \"date_created\": \"Mon, 06 Oct 2014 23:57:11 0000\", \"date_updated\": \"Mon, 06 Oct 2014 23:57:11 0000\", \"friendly_name\": \"Test\", \"iso_country\": \"AU\", \"postal_code\": \"12345\", \"region\": \"NSW\", \"sid\": \"AD79f1863d1ad0c25e795bb7577da0999a\", \"street\": \"123+Main+St\", \"uri\": \"/2010-04-01/Accounts/AC9fa6e6422f53463a89b5d81b2645d070/Addresses/AD79f1863d1ad0c25e795bb7577da0999a.json\"}";
 
     private static final String LIST_JSON_RESPONSE = "{\"addresses\": [ { \"account_sid\": \"AC9fa6e6422f53463a89b5d81b2645d070\", \"city\": \"Sydney\", \"customer_name\": \"Tester\", \"date_created\": \"Mon, 06 Oct 2014 23:57:11 +0000\", \"date_updated\": \"Mon, 06 Oct 2014 23:57:11 +0000\", \"friendly_name\": \"Test\", \"iso_country\": \"AU\", \"postal_code\": \"12345\", \"region\": \"NSW\", \"sid\": \"AD79f1863d1ad0c25e795bb7577da0999a\", \"street\": \"123+Main+St\", \"uri\": \"/2010-04-01/Accounts/AC9fa6e6422f53463a89b5d81b2645d070/Addresses/AD79f1863d1ad0c25e795bb7577da0999a.json\" } ], \"end\": 0, \"first_page_uri\": \"/2010-04-01/Accounts/AC9fa6e6422f53463a89b5d81b2645d070/Addresses.json?PageSize=50&Page=0\", \"last_page_uri\": \"/2010-04-01/Accounts/AC9fa6e6422f53463a89b5d81b2645d070/Addresses.json?PageSize=50&Page=0\", \"next_page_uri\": null, \"num_pages\": 1, \"page\": 0, \"page_size\": 50, \"previous_page_uri\": null, \"start\": 0, \"total\": 1, \"uri\": \"/2010-04-01/Accounts/AC9fa6e6422f53463a89b5d81b2645d070/Addresses.json?PageSize=50&Page=0\"}";
 
@@ -27,6 +27,18 @@ public class AddressTest {
     @Before
     public void setUp() throws Exception {
         Twilio.init("AC123", "AUTH TOKEN");
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        new NonStrictExpectations() {{
+            twilioRestClient.request((Request) any);
+            result = new Response("", TwilioRestClient.HTTP_STATUS_CODE_NO_CONTENT);
+            request.constructURL(); result = "https://api.twilio.com/2010-04-01/Addresses/sid.json".replace("{AccountSid}", "AC123");
+            twilioRestClient.getObjectMapper(); result = new ObjectMapper();
+        }};
+
+        Address.delete("sid").execute();
     }
 
     @Test
@@ -52,7 +64,6 @@ public class AddressTest {
         assertNotNull(instance);
     }
 
-    
     @Test
     public void testList() throws Exception {
         new NonStrictExpectations() {{
@@ -64,5 +75,18 @@ public class AddressTest {
         }};
 
         Address.list().execute();
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        new NonStrictExpectations() {{
+            twilioRestClient.request((Request) any);
+            result = new Response(INSTANCE_JSON_RESPONSE, TwilioRestClient.HTTP_STATUS_CODE_OK);
+            request.constructURL(); result = "https://api.twilio.com/2010-04-01/Addresses/sid.json".replace("{AccountSid}", "AC123");
+            twilioRestClient.getObjectMapper(); result = new ObjectMapper();
+        }};
+
+        Address.update("sid")
+            .execute();
     }
 }

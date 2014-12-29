b3b4c88bd44ceeea38cd3c30af41d3f4fddfb1b0
Sam Kimbrel <skimbrel@twilio.com>
2014-12-29 15:12:18 -0800

    Test pricing resources

---
+++ src/test/java/com/twilio/sdk/resource/instance/pricing/PhoneNumberCountryTest.java
@@ -0,0 +1,41 @@
+package com.twilio.sdk.resource.instance.pricing;
+
+import com.sun.org.apache.xpath.internal.operations.*;
+import com.twilio.sdk.resource.instance.BasicRequestTester;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class PhoneNumberCountryTest extends BasicRequestTester {
+
+    @Before
+    public void setup() throws Exception {
+        setExpectedServerContentType("application/json");
+        setExpectedServerAnswer("/" + getClass().getPackage().getName().replace(".", "/") + "/phone_number_country.json");
+    }
+
+    @Test
+    public void testGetPhoneNumberCountry() throws Exception {
+        setExpectedServerReturnCode(200);
+
+        PhoneNumberCountry country = pricingClient.getPhoneNumberCountry("EE");
+        assertNotNull(country);
+
+        assertEquals("Estonia", country.getCountry());
+        assertEquals("EE", country.getIsoCountry());
+        assertEquals("usd", country.getPriceUnit());
+
+        List<PhoneNumberCountry.NumberPrice> numberPrices = country.getPhoneNumberPrices();
+        assertEquals(2, numberPrices.size());
+
+        PhoneNumberCountry.NumberPrice price = numberPrices.get(0);
+        assertEquals(NumberType.MOBILE, price.getNumberType());
+        assertEquals(new BigDecimal("3.00"), price.getBasePrice());
+        assertEquals(new BigDecimal("3.00"), price.getCurrentPrice());
+    }
+}
---
+++ src/test/java/com/twilio/sdk/resource/instance/pricing/VoiceCountryTest.java
@@ -0,0 +1,48 @@
+package com.twilio.sdk.resource.instance.pricing;
+
+import com.twilio.sdk.resource.instance.BasicRequestTester;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class VoiceCountryTest extends BasicRequestTester {
+
+    @Before
+    public void setup() throws Exception {
+        setExpectedServerContentType("application/json");
+        setExpectedServerAnswer("/" + getClass().getPackage().getName().replace(".", "/") + "/voice_country.json");
+    }
+
+    @Test
+    public void testGetVoiceCountry() throws Exception {
+        setExpectedServerReturnCode(200);
+        VoiceCountry country = pricingClient.getVoiceCountry("EE");
+        assertNotNull(country);
+        assertEquals("Estonia", country.getCountry());
+        assertEquals("EE", country.getIsoCountry());
+        assertEquals("usd", country.getPriceUnit());
+
+        List<VoiceCountry.OutboundPrefixPrice> outboundPrefixPrices = country.getOutboundPrefixPrices();
+        assertEquals(3, outboundPrefixPrices.size());
+
+        VoiceCountry.OutboundPrefixPrice outboundPrefixPrice = outboundPrefixPrices.get(0);
+        assertEquals("Programmable Outbound Minute - Estonia", outboundPrefixPrice.getFriendlyName());
+        assertEquals("372", outboundPrefixPrice.getPrefixes().get(0));
+        assertEquals(new BigDecimal("0.033"), outboundPrefixPrice.getCallBasePrice());
+        assertEquals(new BigDecimal("0.030"), outboundPrefixPrice.getCallCurrentPrice());
+
+        List<VoiceCountry.InboundCallPrice> inboundCallPrices = country.getInboundCallPrices();
+        assertEquals(2, inboundCallPrices.size());
+
+        VoiceCountry.InboundCallPrice inboundCallPrice = inboundCallPrices.get(0);
+        assertEquals(NumberType.MOBILE, inboundCallPrice.getNumberType());
+        assertEquals(new BigDecimal("0.0075"), inboundCallPrice.getCallBasePrice());
+        assertEquals(new BigDecimal("0.0070"), inboundCallPrice.getCallCurrentPrice());
+    }
+}

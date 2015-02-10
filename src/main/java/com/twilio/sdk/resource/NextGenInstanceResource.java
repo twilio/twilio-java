f691c7bcfa1db7c0b7f72124a19a33beb795ff2a
Sam Kimbrel <skimbrel@twilio.com>
2015-02-10 11:04:59 -0800

    Implement next-gen resource wrappers

---
+++ src/main/java/com/twilio/sdk/resource/NextGenInstanceResource.java
@@ -0,0 +1,38 @@
+package com.twilio.sdk.resource;
+
+import com.twilio.sdk.TwilioClient;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+public abstract class NextGenInstanceResource<C extends TwilioClient> extends InstanceResource {
+
+	protected static final SimpleDateFormat ISO_8601_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+	public NextGenInstanceResource(final C client) {
+		super(client);
+	}
+
+	public NextGenInstanceResource(final C client, final Map<String, Object> properties) {
+		super(client, properties);
+	}
+
+	public NextGenInstanceResource(final C client, final Map<String, Object> properties, final Map<String, Object> filters) {
+		super(client, properties, filters);
+	}
+
+	@Override
+	protected Date parseDate(final String inDate) {
+		if (inDate == null) {
+			return null;
+		}
+
+		try {
+			return ISO_8601_DATE_FORMAT.parse(inDate);
+		} catch (ParseException e) {
+			return null;
+		}
+	}
+}

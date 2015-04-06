5e6132639f0fcb78ed49fb359201e7f02a37ad13
Sam Kimbrel <skimbrel@twilio.com>
2015-04-06 14:21:28 -0700

    Add support for StatusCallbackEvent

--- src/main/java/com/twilio/sdk/verbs/Sip.java
+++ src/main/java/com/twilio/sdk/verbs/Sip.java
@@ -46,4 +46,43 @@ public class Sip extends Verb {
     public void setMethod(String method){
        this.set("method", method);
     }
+
+	/**
+	 * Set a URL to be requested when the status of this Call changes.
+	 * @param url Status callback URL.
+	 */
+	public void setStatusCallback(String url) {
+		this.set("statusCallback", url);
+	}
+
+	/**
+	 * Set the method Twilio will use to request the StatusCallback URL.
+	 * @param method Either "GET" or "POST"
+	 */
+	public void setStatusCallbackMethod(String method) {
+		this.set("statusCallbackMethod", method);
+	}
+
+	/**
+	 * Set the events Twilio will fire StatusCallbacks for.
+	 * Available call lifecycle events:
+	 * <ul>
+	 *     <li>initiated</li>
+	 *     <li>ringing</li>
+	 *     <li>answered</li>
+	 *     <li>completed</li>
+	 * </ul>
+	 *
+	 * 'completed' events are provided gratis; for pricing on the other
+	 * events see twilio.com.
+	 *
+	 * To get callbacks for more than one event, provide a space-separated
+	 * list of the events, for example "initiated ringing completed".
+	 *
+	 * If unset, Twilio defaults to sending only the 'completed' event.
+	 * @param events Events to provide callbacks for.
+	 */
+	public void setStatusCallbackEvents(String events) {
+		this.set("statusCallbackEvents", events);
+	}
 }

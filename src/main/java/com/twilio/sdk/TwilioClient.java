0684d9e703eed626258a44c127694228b450faaf
Sam Kimbrel <skimbrel@twilio.com>
2015-06-09 10:26:23 -0700

    Bump to 4.4.1

--- src/main/java/com/twilio/sdk/TwilioClient.java
+++ src/main/java/com/twilio/sdk/TwilioClient.java
@@ -43,7 +43,7 @@ public abstract class TwilioClient {
 	private static final int AUTH_TOKEN_LENGTH = 32;
 
 	/** The Constant VERSION. */
-	private static final String VERSION = "4.4.0";
+	private static final String VERSION = "4.4.1";
 
 	/** The endpoint. */
 	private String endpoint = "https://api.twilio.com";

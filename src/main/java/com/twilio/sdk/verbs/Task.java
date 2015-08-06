63ca7ad0608a5f40295cc4f79859edd0b265195c
Justin Witz <jwitz@twilio.com>
2015-08-06 12:58:58 -0700

    Fix formatting

--- src/main/java/com/twilio/sdk/verbs/Task.java
+++ src/main/java/com/twilio/sdk/verbs/Task.java
@@ -6,20 +6,19 @@ import org.json.simple.JSONObject;
 
 public class Task extends Verb {
 
-	public Task(String attributes) {
-		super("Task", attributes);
-	}
-	
-	public Task(Map<String, String> attributes) {
-		super("Task", JSONObject.toJSONString(attributes));
-	}
-	
-	public void setPriority(final int priority) {
+    public Task(String attributes) {
+        super("Task", attributes);
+    }
+
+    public Task(Map<String, String> attributes) {
+        super("Task", JSONObject.toJSONString(attributes));
+    }
+
+    public void setPriority(final int priority) {
         this.set("priority", String.valueOf(priority));
     }
 
     public void setTimeout(final int timeout) {
         this.set("timeout", String.valueOf(timeout));
     }
-
 }

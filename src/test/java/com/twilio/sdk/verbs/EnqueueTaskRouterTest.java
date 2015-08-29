c5fc00c837abfce43bfa65f965cba8c2e7a9f69a
Justin Witz <jwitz@twilio.com>
2015-08-28 17:02:32 -0700

    Add tests for n/a Enqueue constructor for TaskRouter TwiML

--- src/test/java/com/twilio/sdk/verbs/EnqueueTaskRouterTest.java
+++ src/test/java/com/twilio/sdk/verbs/EnqueueTaskRouterTest.java
@@ -87,7 +87,7 @@ public class EnqueueTaskRouterTest {
 
     @Test
     /**
-     * Test Enqueuing a Task with json attributes, dynamic prioirty and timeout
+     * Test Enqueuing a Task with json attributes, dynamic priority and timeout
      */
     public void testEnqueueTaskWithAllAttributes() throws TwiMLException {
         Enqueue enq = new Enqueue();
@@ -102,6 +102,51 @@ public class EnqueueTaskRouterTest {
         enq.append(task);
 
         assertEquals("<Enqueue workflowSid=\"WFxxxx\"><Task priority=\"10\" timeout=\"30\">{\"selected_language\":\"en\"}</Task></Enqueue>", enq.toXML());
+    }
+    
+    @Test
+    /**
+     * Test Enqueuing a Task with different constructor with empty queue name and empty json
+     */
+    public void testEnqueueTaskEmptyQueue() throws TwiMLException {
+        Enqueue enq = new Enqueue("");
+        enq.setWorkflowSid("WFxxxx");
+
+        String json = "{}";
+        Task task = new Task(json);
+        enq.append(task);
+
+        assertEquals("<Enqueue workflowSid=\"WFxxxx\"><Task>{}</Task></Enqueue>", enq.toXML());
+    }
+    
+    @Test
+    /**
+     * Test Enqueuing a Task with different constructor with null queue name and empty json
+     */
+    public void testEnqueueTaskNullQueue() throws TwiMLException {
+        Enqueue enq = new Enqueue(null);
+        enq.setWorkflowSid("WFxxxx");
+
+        String json = "{}";
+        Task task = new Task(json);
+        enq.append(task);
+
+        assertEquals("<Enqueue workflowSid=\"WFxxxx\"><Task>{}</Task></Enqueue>", enq.toXML());
+    }
+    
+    @Test
+    /**
+     * Test Enqueuing a Task with different constructor with n/a queue name and empty json
+     */
+    public void testEnqueueTaskRandomInvalidQueue() throws TwiMLException {
+        Enqueue enq = new Enqueue("NotUsedQueue");
+        enq.setWorkflowSid("WFxxxx");
+
+        String json = "{}";
+        Task task = new Task(json);
+        enq.append(task);
+
+        assertEquals("<Enqueue workflowSid=\"WFxxxx\">NotUsedQueue<Task>{}</Task></Enqueue>", enq.toXML());
     }
 
 }

5a4d2952ffcb87c2220e2229baa6532b56af2337
Jingming Niu <jniu@twilio.com>
2015-09-17 13:37:44 -0700

    Remove jniu javadoc

--- src/main/java/com/twilio/sdk/resource/list/pricing/MessagingCountryList.java
+++ src/main/java/com/twilio/sdk/resource/list/pricing/MessagingCountryList.java
@@ -4,11 +4,23 @@ import com.twilio.sdk.TwilioPricingClient;
 import com.twilio.sdk.resource.NextGenListResource;
 import com.twilio.sdk.resource.instance.pricing.MessagingCountry;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by jniu on 9/14/15.
+ * A list of MessagingCountry objects where Twilio Phone Numbers are available.
+ *
+ * The returned MessagingCountry objects will not have pricing information populated.
+ * To retrieve pricing information for a specific country, request it as follows:
+ *
+ * <code>
+ *     TwilioPricingClient client = new TwilioPricingClient("ACCOUNT SID", "AUTH TOKEN");
+ *     // Retrieve country-specific pricing info
+ *     MessagingCountry country = client.getMessagingCountry("US");
+ *     // Print number prices by type
+ *     for (MessagingCountry.MessagingPrice p : country.getInboundSmsPrices()) {
+ *         System.out.println(p.getNumberType().toString() + ": " + p.getBasePrice().toString());
+ *     }
+ * </code>
  */
 public class MessagingCountryList extends NextGenListResource<MessagingCountry, TwilioPricingClient> {
 

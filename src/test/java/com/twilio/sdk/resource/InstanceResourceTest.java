7857f2a1dbe4303f6c95eb3c2df1c7a9f10d91df
Senthil Ramakrishnan <sramakrishnan@twilio.com>
2015-12-09 15:15:34 -0800

    Added test for the helper method getPropertyAsBoolean

---
+++ src/test/java/com/twilio/sdk/resource/InstanceResourceTest.java
@@ -0,0 +1,72 @@
+package com.twilio.sdk.resource;
+
+import java.util.HashMap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.twilio.sdk.TwilioIPMessagingClient;
+import com.twilio.sdk.resource.instance.ipmessaging.Message;
+
+public class InstanceResourceTest {
+
+	@Test
+	public void testGetPropertyAsBoolean() {
+
+		Message resource = new Message(new TwilioIPMessagingClient("ACSid",
+				"AuthToken"), new HashMap<String, Object>()) {
+
+			public String getProperty(String name) {
+				return "true";
+			};
+
+		};
+		Boolean actual = resource.getPropertyAsBoolean("read_status_enabled");
+		Assert.assertTrue(actual);
+	}
+
+	@Test
+	public void testGetPropertyAsBooleanForNullValue() {
+
+		Message resource = new Message(new TwilioIPMessagingClient("ACSid",
+				"AuthToken"), new HashMap<String, Object>()) {
+
+			public String getProperty(String name) {
+				return null;
+			};
+
+		};
+		Boolean actual = resource.getPropertyAsBoolean("read_status_enabled");
+		Assert.assertFalse(actual);
+	}
+
+	@Test
+	public void testGetPropertyAsBooleanForEmptyValue() {
+
+		Message resource = new Message(new TwilioIPMessagingClient("ACSid",
+				"AuthToken"), new HashMap<String, Object>()) {
+
+			public String getProperty(String name) {
+				return "";
+			};
+
+		};
+		Boolean actual = resource.getPropertyAsBoolean("read_status_enabled");
+		Assert.assertFalse(actual);
+	}
+
+	@Test
+	public void testGetPropertyAsBooleanForNonBooleanValue() {
+
+		Message resource = new Message(new TwilioIPMessagingClient("ACSid",
+				"AuthToken"), new HashMap<String, Object>()) {
+
+			public String getProperty(String name) {
+				return "random";
+			};
+
+		};
+		Boolean actual = resource.getPropertyAsBoolean("read_status_enabled");
+		Assert.assertFalse(actual);
+	}
+}

bf82c2079ac6d292e6a5dc1128aefac7e0d8d9bc
Jingming Niu <niu@jingming.ca>
2016-03-28 10:29:34 -0700

    Remove commented stuff

--- src/main/java/com/twilio/sdk/taskrouter/FilterRequirement.java
+++ src/main/java/com/twilio/sdk/taskrouter/FilterRequirement.java
@@ -24,13 +24,3 @@ public enum FilterRequirement {
     }
 }
 
-//public class FilterRequirement extends HashMap<String, Boolean> {
-//
-//    public FilterRequirement(final boolean required) {
-//        super();
-//        put("required", required);
-//    }
-//
-//    public static FilterRequirement REQUIRED = new FilterRequirement(true);
-//    public static FilterRequirement OPTIONAL = new FilterRequirement(false);
-//}

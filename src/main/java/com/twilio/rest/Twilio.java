ad29bb86cb6a04058be2e7ab245433c003921c30
Jingming Niu <niu@jingming.ca>
2016-08-02 14:48:45 -0700

    Bump version

--- src/main/java/com/twilio/rest/Twilio.java
+++ src/main/java/com/twilio/rest/Twilio.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Executors;
  */
 public class Twilio {
 
-    public static final String VERSION = "7.0.0-rc-15";
+    public static final String VERSION = "7.0.0-rc-16";
 
     private static String username;
     private static String password;

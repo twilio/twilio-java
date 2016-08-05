21995ed3c60294711c45f4dbd32797dd71028b7e
Jingming Niu <niu@jingming.ca>
2016-08-05 13:38:51 -0700

    Add programmable voice grant

---
+++ src/main/java/com/twilio/sdk/auth/ProgrammableVoiceGrant.java
@@ -0,0 +1,85 @@
+package com.twilio.sdk.auth;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Grant used to access Twilio Conversations.
+ *
+ * <p>
+ *     For more information see:
+ *     <a href="https://www.twilio.com/docs/api/rest/access-tokens">
+ *         https://www.twilio.com/docs/api/rest/access-tokens
+ *     </a>
+ * </p>
+ */
+public class ProgrammableVoiceGrant implements Grant {
+
+    private String outgoingApplicationSid;
+    private Map<String, Object> outgoingApplicationParams;
+    private String pushCredentialSid;
+    private String endpointId;
+
+    public ProgrammableVoiceGrant setOutgoingApplicationSid(String outgoingApplicationSid) {
+        this.outgoingApplicationSid = outgoingApplicationSid;
+        return this;
+    }
+
+    public ProgrammableVoiceGrant setOutgoingApplication(
+        String outgoingApplicationSid,
+        Map<String, Object> outgoingApplicationParams
+    ) {
+        this.outgoingApplicationSid = outgoingApplicationSid;
+        this.outgoingApplicationParams = outgoingApplicationParams;
+        return this;
+    }
+
+    public ProgrammableVoiceGrant setPushCredentialSid(String pushCredentialSid) {
+        this.pushCredentialSid = pushCredentialSid;
+        return this;
+    }
+
+    public ProgrammableVoiceGrant setEndpointId(String endpointId) {
+        this.endpointId = endpointId;
+        return this;
+    }
+
+    @Override
+    public String getGrantKey() {
+        return "programmable_voice";
+    }
+
+    @Override
+    public Object getPayload() {
+        return new Payload(this);
+    }
+
+    @SuppressWarnings("checkstyle:membername")
+    public class Payload {
+        public Map<String, Object> outgoing;
+        public String push_credential_sid;
+        public String endpoint_id;
+
+        public Payload(ProgrammableVoiceGrant grant) {
+            if (!StringUtils.isEmpty(grant.outgoingApplicationSid)) {
+                this.outgoing = new HashMap<String, Object>();
+                this.outgoing.put("application_sid", grant.outgoingApplicationSid);
+
+                if (grant.outgoingApplicationParams != null) {
+                    this.outgoing.put("params", grant.outgoingApplicationParams);
+                }
+            }
+
+            if (!StringUtils.isEmpty(grant.pushCredentialSid)) {
+                this.push_credential_sid = grant.pushCredentialSid;
+            }
+
+            if (!StringUtils.isEmpty(grant.endpointId)) {
+                this.endpoint_id = grant.endpointId;
+            }
+        }
+    }
+
+}
--- src/test/java/com/twilio/sdk/AccessTokenTest.java
+++ src/test/java/com/twilio/sdk/AccessTokenTest.java
@@ -3,137 +3,191 @@ package com.twilio.sdk;
 import com.twilio.sdk.auth.AccessToken;
 import com.twilio.sdk.auth.ConversationsGrant;
 import com.twilio.sdk.auth.IpMessagingGrant;
+import com.twilio.sdk.auth.ProgrammableVoiceGrant;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
+/**
+ * Test class for {@link AccessToken}.
+ */
 public class AccessTokenTest {
 
-	public static final String SIGNING_KEY_SID = "SK123";
-	public static final String ACCOUNT_SID = "AC123";
-	public static final String SECRET = "secret";
+	private static final String ACCOUNT_SID = "AC123";
+	private static final String SIGNING_KEY_SID = "SK123";
+	private static final String SECRET = "secret";
 
-	protected void validateClaims(Claims claims) {
-		assertEquals(SIGNING_KEY_SID, claims.getIssuer());
-		assertEquals(ACCOUNT_SID, claims.getSubject());
+	private void validateToken(Claims claims) {
+		Assert.assertEquals(SIGNING_KEY_SID, claims.getIssuer());
+		Assert.assertEquals(ACCOUNT_SID, claims.getSubject());
 
-		assertNotNull(claims.getExpiration());
-		assertNotNull(claims.getId());
-		assertNotNull(claims.get("grants"));
+		Assert.assertNotNull(claims.getExpiration());
+		Assert.assertNotNull(claims.getId());
+		Assert.assertNotNull(claims.get("grants"));
 
-		assertTrue(claims.getId().startsWith(claims.getIssuer() + "-"));
-		assertTrue(claims.getExpiration().getTime() > new Date().getTime());
+		Assert.assertTrue(claims.getId().startsWith(claims.getIssuer() + "-"));
+		Assert.assertTrue(claims.getExpiration().getTime() > new Date().getTime());
 	}
 
 	@Test
-	public void testEmptyGrants() {
-		AccessToken accessToken =
+	public void testEmptyToken() {
+		AccessToken token =
 			new AccessToken.Builder(ACCOUNT_SID, SIGNING_KEY_SID, SECRET)
 				.build();
 
 		Claims claims =
 			Jwts.parser()
 				.setSigningKey(SECRET.getBytes())
-				.parseClaimsJws(accessToken.toJWT())
+				.parseClaimsJws(token.toJWT())
 				.getBody();
 
-		this.validateClaims(claims);
+		validateToken(claims);
 	}
 
 	@Test
-	public void testNbf() {
+	public void testOptionalValues() {
 		Date now = new Date();
-		int timestamp = (int) (Math.floor(now.getTime() / 1000.0f)) - 300;
-		AccessToken accessToken =
+		AccessToken token =
 			new AccessToken.Builder(ACCOUNT_SID, SIGNING_KEY_SID, SECRET)
-				.nbf(timestamp)
+				.identity(ACCOUNT_SID)
+				.nbf((int) (Math.floor(now.getTime() / 1000.0f)) - 300)
 				.build();
 
 		Claims claims =
 			Jwts.parser()
 				.setSigningKey(SECRET.getBytes())
-				.parseClaimsJws(accessToken.toJWT())
+				.parseClaimsJws(token.toJWT())
 				.getBody();
 
-		this.validateClaims(claims);
-
-		// Just check up to the second
-		assertEquals(timestamp, (int) (claims.getNotBefore().getTime() / 1000));
-	}
-
-	@Test
-	public void testSingleGrant() {
-		IpMessagingGrant grant = new IpMessagingGrant();
-		grant.setPushCredentialSid("credentialSid")
-				.setEndpointId("endpointId")
-				.setDeploymentRoleSid("roleSid")
-				.setServiceSid("serviceSid");
-
-		AccessToken accessToken =
-			new AccessToken.Builder(ACCOUNT_SID, SIGNING_KEY_SID, SECRET)
-				.identity("ID@example.com")
-				.grant(grant)
-				.build();
-
-		Claims claims = Jwts.parser()
-							.setSigningKey(SECRET.getBytes())
-							.parseClaimsJws(accessToken.toJWT())
-							.getBody();
-
-		this.validateClaims(claims);
-
-		Map<String, Object> decodedGrants = (Map<String, Object>) claims.get("grants");
-		assertEquals(2, decodedGrants.size());
-
-		Map<String, Object> payload = (Map<String, Object>) decodedGrants.get("ip_messaging");
-		assertEquals("serviceSid", payload.get("service_sid"));
-		assertEquals("roleSid", payload.get("deployment_role_sid"));
-		assertEquals("endpointId", payload.get("endpoint_id"));
-		assertEquals("credentialSid", payload.get("push_credential_sid"));
+		validateToken(claims);
+		Assert.assertTrue(claims.getNotBefore().getTime() <= new Date().getTime());
 	}
 
 	@Test
 	public void testConversationGrant() {
-		AccessToken accessToken =
+		ConversationsGrant cg = new ConversationsGrant().setConfigurationProfileSid("CP123");
+		AccessToken token =
 			new AccessToken.Builder(ACCOUNT_SID, SIGNING_KEY_SID, SECRET)
-				.grant(new ConversationsGrant())
+				.grant(cg)
 				.build();
 
-		Claims claims = Jwts.parser()
+		Claims claims =
+			Jwts.parser()
 				.setSigningKey(SECRET.getBytes())
-				.parseClaimsJws(accessToken.toJWT())
+				.parseClaimsJws(token.toJWT())
 				.getBody();
 
-		this.validateClaims(claims);
-		Map<String, Object> decodedGrants = (Map<String, Object>) claims.get("grants");
-		assertEquals(1, decodedGrants.size());
+		validateToken(claims);
 
-		assertTrue("should be a rtc grant present", decodedGrants.containsKey("rtc"));
+		Map<String, Object> decodedGrants = (Map<String, Object>) claims.get("grants");
+		Assert.assertEquals(1, decodedGrants.size());
+
+		Map<String, Object> grant = (Map<String, Object>) decodedGrants.get("rtc");
+		Assert.assertEquals("CP123", grant.get("configuration_profile_sid"));
 	}
 
 	@Test
 	public void testIpMessagingGrant() {
-		AccessToken accessToken =
+		IpMessagingGrant ipg = new IpMessagingGrant()
+			.setDeploymentRoleSid("RL123")
+			.setEndpointId("foobar")
+			.setPushCredentialSid("CR123")
+			.setServiceSid("IS123");
+		AccessToken token =
 			new AccessToken.Builder(ACCOUNT_SID, SIGNING_KEY_SID, SECRET)
-				.grant(new IpMessagingGrant())
+				.grant(ipg)
 				.build();
 
-		Claims claims = Jwts.parser()
+		Claims claims =
+			Jwts.parser()
 				.setSigningKey(SECRET.getBytes())
-				.parseClaimsJws(accessToken.toJWT())
+				.parseClaimsJws(token.toJWT())
 				.getBody();
 
-		this.validateClaims(claims);
-		Map<String, Object> decodedGrants = (Map<String, Object>) claims.get("grants");
-		assertEquals(1, decodedGrants.size());
+		validateToken(claims);
 
-		assertTrue("should be a ip_messaging grant present", decodedGrants.containsKey("ip_messaging"));
+		Map<String, Object> decodedGrants = (Map<String, Object>) claims.get("grants");
+		Assert.assertEquals(1, decodedGrants.size());
+
+		Map<String, Object> grant = (Map<String, Object>) decodedGrants.get("ip_messaging");
+		Assert.assertEquals("RL123", grant.get("deployment_role_sid"));
+		Assert.assertEquals("foobar", grant.get("endpoint_id"));
+		Assert.assertEquals("CR123", grant.get("push_credential_sid"));
+		Assert.assertEquals("IS123", grant.get("service_sid"));
+	}
+
+	@Test
+	public void testCompleteToken() {
+		IpMessagingGrant ipg = new IpMessagingGrant()
+			.setDeploymentRoleSid("RL123")
+			.setEndpointId("foobar")
+			.setPushCredentialSid("CR123")
+			.setServiceSid("IS123");
+
+		ConversationsGrant cg = new ConversationsGrant().setConfigurationProfileSid("CP123");
+
+		Date now = new Date();
+		AccessToken token =
+			new AccessToken.Builder(ACCOUNT_SID, SIGNING_KEY_SID, SECRET)
+				.grant(ipg)
+				.grant(cg)
+				.nbf((int) (Math.floor(now.getTime() / 1000.0f)) - 300)
+				.build();
+
+		Claims claims =
+			Jwts.parser()
+				.setSigningKey(SECRET.getBytes())
+				.parseClaimsJws(token.toJWT())
+				.getBody();
+
+		validateToken(claims);
+		Assert.assertTrue(claims.getNotBefore().getTime() <= new Date().getTime());
+
+		Map<String, Object> decodedGrants = (Map<String, Object>) claims.get("grants");
+		Assert.assertEquals(2, decodedGrants.size());
+
+		Map<String, Object> ipmGrant = (Map<String, Object>) decodedGrants.get("ip_messaging");
+		Assert.assertEquals("RL123", ipmGrant.get("deployment_role_sid"));
+		Assert.assertEquals("foobar", ipmGrant.get("endpoint_id"));
+		Assert.assertEquals("CR123", ipmGrant.get("push_credential_sid"));
+		Assert.assertEquals("IS123", ipmGrant.get("service_sid"));
+
+		Map<String, Object> cGrant = (Map<String, Object>) decodedGrants.get("rtc");
+		Assert.assertEquals("CP123", cGrant.get("configuration_profile_sid"));
+	}
+
+	@Test
+	public void testProgrammableVoiceToken() {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("foo", "bar");
+
+		ProgrammableVoiceGrant pvg = new ProgrammableVoiceGrant()
+			.setOutgoingApplication("AP123", params);
+
+		AccessToken token =
+			new AccessToken.Builder(ACCOUNT_SID, SIGNING_KEY_SID, SECRET)
+				.grant(pvg)
+				.build();
+
+		Claims claims =
+			Jwts.parser()
+				.setSigningKey(SECRET.getBytes())
+				.parseClaimsJws(token.toJWT())
+				.getBody();
+
+		validateToken(claims);
+		Map<String, Object> decodedGrants = (Map<String, Object>) claims.get("grants");
+		Assert.assertEquals(1, decodedGrants.size());
+
+		Map<String, Object> pvgGrant = (Map<String, Object>) decodedGrants.get("programmable_voice");
+		Map<String, Object> outgoing = (Map<String, Object>) pvgGrant.get("outgoing");
+		Map<String, Object> outgoingParams = (Map<String, Object>) outgoing.get("params");
+		Assert.assertEquals("AP123", outgoing.get("application_sid"));
+		Assert.assertEquals("bar", outgoingParams.get("foo"));
 	}
 }

a7a950495532d4b681726e6a4cc9ea4c7d9185e5
Jingming Niu <niu@jingming.ca>
2016-10-04 13:30:56 -0700

    Generate edge artifact

--- src/main/java/com/twilio/rest/Domains.java
+++ src/main/java/com/twilio/rest/Domains.java
@@ -13,6 +13,8 @@ public enum Domains {
     IPMESSAGING("ip-messaging"),
     LOOKUPS("lookups"),
     MONITOR("monitor"),
+    NOTIFY("notify"),
+    PREVIEW("preview"),
     PRICING("pricing"),
     TASKROUTER("taskrouter"),
     TRUNKING("trunking");

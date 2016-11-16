c6925c18e99958936be3a307cdda652c8e1a3c17
Jingming Niu <niu@jingming.ca>
2016-11-16 15:11:29 -0800

    Fix the tests

--- src/main/java/com/twilio/jwt/Jwt.java
+++ src/main/java/com/twilio/jwt/Jwt.java
@@ -4,8 +4,8 @@ import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
-import java.nio.charset.StandardCharsets;
-import java.security.PrivateKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,8 +16,7 @@ import java.util.Map;
 public abstract class Jwt {
 
     private final SignatureAlgorithm algorithm;
-    private final PrivateKey secretKey;
-    private final String secret;
+    private final Key secretKey;
     private final String issuer;
     private final Date expiration;
 
@@ -35,21 +34,29 @@ public abstract class Jwt {
         String issuer,
         Date expiration
     ) {
-        this.algorithm = algorithm;
-        this.secret = secret;
-        this.secretKey = null;
-        this.issuer = issuer;
-        this.expiration = expiration;
+        this(
+            algorithm,
+            new SecretKeySpec(secret.getBytes(), algorithm.getJcaName()),
+            issuer,
+            expiration
+        );
     }
 
+    /**
+     * Create a new JWT.
+     *
+     * @param algorithm algorithm to use
+     * @param secretKey secret key
+     * @param issuer JWT issuer
+     * @param expiration expiration Date
+     */
     public Jwt(
         SignatureAlgorithm algorithm,
-        PrivateKey secretKey,
+        Key secretKey,
         String issuer,
         Date expiration
     ) {
         this.algorithm = algorithm;
-        this.secret = null;
         this.secretKey = secretKey;
         this.issuer = issuer;
         this.expiration = expiration;
@@ -67,15 +74,10 @@ public abstract class Jwt {
 
         JwtBuilder builder =
             Jwts.builder()
+                .signWith(this.algorithm, this.secretKey)
                 .setHeaderParams(headers)
                 .setIssuer(this.issuer)
                 .setExpiration(expiration);
-
-        if (this.secret != null) {
-            builder.signWith(this.algorithm, this.secret.getBytes(StandardCharsets.UTF_8));
-        } else if (this.secretKey != null) {
-            builder.signWith(this.algorithm, this.secretKey);
-        }
 
         if (this.getClaims() != null) {
             for (Map.Entry<String, Object> entry : this.getClaims().entrySet()) {

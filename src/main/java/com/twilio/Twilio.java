fb7e823993309347f59a12fa55641565bc16b7e5
Evan Fossier <efossier@twilio.com>
2017-02-16 16:31:02 -0800

    "Regenerate Library"

--- src/main/java/com/twilio/Twilio.java
+++ src/main/java/com/twilio/Twilio.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Executors;
  */
 public class Twilio {
 
-    public static final String VERSION = "7.5.0-alpha-2";
+    public static final String VERSION = "7.5.0-alpha-3";
     public static final String JAVA_VERSION = System.getProperty("java.version");
 
     private static String username;

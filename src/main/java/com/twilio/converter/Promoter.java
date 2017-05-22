6ef5f3be3444368dd5d239cd1a4f6adf4ecfab37
Jingming Niu <niu@jingming.ca>
2017-05-22 13:56:23 -0700

    Merge master into alpha

--- src/main/java/com/twilio/converter/Promoter.java
+++ src/main/java/com/twilio/converter/Promoter.java
@@ -55,4 +55,6 @@ public class Promoter {
 
         return null;
     }
+
+
 }
--- src/main/java/com/twilio/jwt/validation/ValidationToken.java
+++ src/main/java/com/twilio/jwt/validation/ValidationToken.java
@@ -3,7 +3,6 @@ package com.twilio.jwt.validation;
 import com.google.common.base.Charsets;
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
-import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
@@ -15,12 +14,10 @@ import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpRequest;
-import org.apache.http.message.BasicHeader;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.PrivateKey;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -32,7 +29,6 @@ public class ValidationToken extends Jwt {
 
     private static final HashFunction HASH_FUNCTION = Hashing.sha256();
     private static final String CTY = "twilio-pkrv;v=1";
-    private static final String NEW_LINE = "\n";
 
     private final String accountSid;
     private final String credentialSid;
@@ -83,48 +79,12 @@ public class ValidationToken extends Jwt {
         String includedHeaders = Joiner.on(";").join(lowercaseSignedHeaders);
         payload.put("hrh", includedHeaders);
 
-        // Add the method and uri
-        StringBuilder signature = new StringBuilder();
-        signature.append(method).append(NEW_LINE);
-        signature.append(uri).append(NEW_LINE);
-
-        // Get the query args, sort and rejoin
-        String[] queryArgs = queryString.split("&");
-        Arrays.sort(queryArgs);
-        String sortedQueryString = Joiner.on("&").join(queryArgs);
-        signature.append(sortedQueryString).append(NEW_LINE);
-
-        // Normalize all the headers
-        Header[] lowercaseHeaders = LOWERCASE_KEYS.apply(headers);
-        Map<String, List<String>> combinedHeaders = COMBINE_HEADERS.apply(lowercaseHeaders);
-
-        // Add the headers that we care about
-        for (String header : lowercaseSignedHeaders) {
-            String lowercase = header.toLowerCase().trim();
-
-            if (combinedHeaders.containsKey(lowercase)) {
-                List<String> values = combinedHeaders.get(lowercase);
-                Collections.sort(values);
-
-                signature.append(lowercase)
-                    .append(":")
-                    .append(Joiner.on(',').join(values))
-                    .append(NEW_LINE);
-            }
-        }
-        signature.append(NEW_LINE);
-
-        // Mark the headers that we care about
-        signature.append(includedHeaders).append(NEW_LINE);
-
-        // Hash and hex the request payload
-        if (!Strings.isNullOrEmpty(requestBody)) {
-            String hashedPayload = HASH_FUNCTION.hashString(requestBody, Charsets.UTF_8).toString();
-            signature.append(hashedPayload);
-        }
+        String canonicalRequest =
+                new RequestCanonicalizer(method, uri, queryString, requestBody, headers).create(
+                        lowercaseSignedHeaders, HASH_FUNCTION);
 
         // Hash and hex the canonical request
-        String hashedSignature = HASH_FUNCTION.hashString(signature.toString(), Charsets.UTF_8).toString();
+        String hashedSignature = HASH_FUNCTION.hashString(canonicalRequest, Charsets.UTF_8).toString();
         payload.put("rqh", hashedSignature);
 
         return payload;
@@ -162,35 +122,6 @@ public class ValidationToken extends Jwt {
 
         return builder.build();
     }
-
-    private static Function<Header[], Map<String, List<String>>> COMBINE_HEADERS = new Function<Header[], Map<String, List<String>>>() {
-        @Override
-        public Map<String, List<String>> apply(Header[] headers) {
-            Map<String, List<String>> combinedHeaders = new HashMap<>();
-
-            for (Header header : headers)  {
-                if (combinedHeaders.containsKey(header.getName())) {
-                    combinedHeaders.get(header.getName()).add(header.getValue());
-                } else {
-                    combinedHeaders.put(header.getName(), Lists.newArrayList(header.getValue()));
-                }
-            }
-
-            return combinedHeaders;
-        }
-    };
-
-    private static Function<Header[], Header[]> LOWERCASE_KEYS = new Function<Header[], Header[]>() {
-        @Override
-        public Header[] apply(Header[] headers) {
-            Header[] lowercaseHeaders = new Header[headers.length];
-            for (int i = 0; i < headers.length; i++) {
-                lowercaseHeaders[i] = new BasicHeader(headers[i].getName().toLowerCase(), headers[i].getValue());
-            }
-
-            return lowercaseHeaders;
-        }
-    };
 
     private static Function<String, String> LOWERCASE_STRING = new Function<String, String>() {
         @Override

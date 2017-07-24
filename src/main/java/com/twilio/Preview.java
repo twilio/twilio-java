348eff74b1121f2eec28f3e5f4c0e13991311981
Ilan Biala <ilan.biala@gmail.com>
2017-07-24 13:04:28 -0700

    fix

--- src/main/java/com/twilio/Preview.java
+++
@@ -1,1 +0,0 @@
-public @interface Preview extends Beta {}
---
+++ src/main/java/com/twilio/annotations/Preview.java
@@ -0,0 +1,18 @@
+package com.twilio.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({
+  ElementType.ANNOTATION_TYPE,
+  ElementType.CONSTRUCTOR,
+  ElementType.FIELD,
+  ElementType.METHOD,
+  ElementType.TYPE
+})
+@Documented
+public @interface Preview {}

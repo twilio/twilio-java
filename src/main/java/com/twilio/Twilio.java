3b6bde6bdec1c802f2cc2a1a32ef2bc4ae62743d
Ilan Biala <ilan.biala@gmail.com>
2017-07-27 12:12:42 -0700

    Bump version to 7.14.0

--- src/main/java/com/twilio/Twilio.java
+++ src/main/java/com/twilio/Twilio.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Executors;
  */
 public class Twilio {
 
-    public static final String VERSION = "7.13.1";
+    public static final String VERSION = "7.14.0";
     public static final String JAVA_VERSION = System.getProperty("java.version");
 
     private static String username;

896f7fd7a564eacd863760a940c922672f313592
Evan Fossier <evan.fossier@gmail.com>
2017-10-06 18:39:04 -0400

    Bump version to 7.15.1

--- src/main/java/com/twilio/Twilio.java
+++ src/main/java/com/twilio/Twilio.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Executors;
  */
 public class Twilio {
 
-    public static final String VERSION = "7.15.0";
+    public static final String VERSION = "7.15.1";
     public static final String JAVA_VERSION = System.getProperty("java.version");
 
     private static String username;

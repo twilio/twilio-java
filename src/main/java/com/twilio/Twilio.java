a907208c4cb66d723a7656113677a28fd91d0465
Jeremy McEntire <jmcentire@twilio.com>
2017-11-10 18:18:19 -0800

    Bump version to 7.15.6

--- src/main/java/com/twilio/Twilio.java
+++ src/main/java/com/twilio/Twilio.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Executors;
  */
 public class Twilio {
 
-    public static final String VERSION = "7.15.5";
+    public static final String VERSION = "7.15.6";
     public static final String JAVA_VERSION = System.getProperty("java.version");
 
     private static String username;

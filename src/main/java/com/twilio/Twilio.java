9c7e634098e255d2c8b5ef4562484bd03b7d63b0
Twilio <twilio-ci@twilio.com>
2019-02-15 16:21:38 -0800

    Bump version to 7.35.1

--- src/main/java/com/twilio/Twilio.java
+++ src/main/java/com/twilio/Twilio.java
@@ -19,7 +19,7 @@ import java.util.concurrent.Executors;
  */
 public class Twilio {
 
-    public static final String VERSION = "7.35.0";
+    public static final String VERSION = "7.35.1";
     public static final String JAVA_VERSION = System.getProperty("java.version");
 
     private static String username;

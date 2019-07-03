aa4e652b0d326cfac49f7dd54ced5f416887e93f
Falguni Vadodaria <falu_vadodaria@yahoo.in>
2019-07-02 18:13:45 -0700

    VIDEO-674 urls property in ice servers (#469)

--- src/main/java/com/twilio/type/IceServer.java
+++ src/main/java/com/twilio/type/IceServer.java
@@ -16,25 +16,34 @@ public class IceServer {
     private final String credential;
     private final String username;
     private final URI url;
+    private final URI urls;
+
 
     /**
      * Initialize an IceServer.
      *
      * @param credential credentials for the server
-     * @param username user account name
-     * @param url url of server
+     * @param username   user account name
+     * @param url        url of server
+     * @param urls       url of server
      */
     @JsonCreator
     public IceServer(@JsonProperty("credential") final String credential,
-                     @JsonProperty("username") final String username,
-                     @JsonProperty("url") final URI url) {
+            @JsonProperty("username") final String username,
+            @JsonProperty("url") final URI url,
+            @JsonProperty("urls") final URI urls) {
         this.credential = credential;
         this.username = username;
         this.url = url;
+        this.urls = urls;
     }
 
     public URI getUrl() {
         return url;
+    }
+
+    public URI getUrls() {
+        return urls;
     }
 
     public String getUsername() {
@@ -59,20 +68,22 @@ public class IceServer {
 
         return (Objects.equals(credential, other.credential) &&
                 Objects.equals(username, other.username) &&
-                Objects.equals(url, other.url));
+                Objects.equals(url, other.url) &&
+                Objects.equals(urls, other.urls));
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(credential, username, url);
+        return Objects.hash(credential, username, url, urls);
     }
 
     @Override
     public String toString() {
         return MoreObjects.toStringHelper(this)
-                          .add("credential", credential)
-                          .add("username", username)
-                          .add("url", url)
-                          .toString();
+                .add("credential", credential)
+                .add("username", username)
+                .add("url", url)
+                .add("urls", urls)
+                .toString();
     }
 }

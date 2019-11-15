caebeb9f9bb7ec418bce5eb4c336bfecc1102269
Craig Dennis <cdennis@twilio.com>
2019-11-15 09:37:14 -0800

    Adds Java implementation for Twiml type with promotions

--- src/main/java/com/twilio/type/Twiml.java
+++ src/main/java/com/twilio/type/Twiml.java
@@ -1,12 +1,34 @@
 package com.twilio.type;
 
-public interface Twiml {
+import java.util.Objects;
 
-    /**
-     * Returns the TwiML String.
-     *
-     * @return TwiML String
-     */
-    public String getTwiml();
+public class Twiml {
+    private final String rawTwiml;
 
+    public Twiml(String twiml) {
+        this.rawTwiml = twiml;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Twiml other = (Twiml) o;
+        return Objects.equals(this.rawTwiml, other.rawTwiml);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.rawTwiml);
+    }
+
+    @Override
+    public String toString() {
+        return this.rawTwiml;
+    }
 }

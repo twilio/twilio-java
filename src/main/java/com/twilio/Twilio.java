5cae2e87ccc496e79fa5243189fc3a529f63dce4
Twilio <twilio-ci@twilio.com>
2019-12-04 22:28:36 +0000

    Release 7.46.0

--- src/main/java/com/twilio/Twilio.java
+++ src/main/java/com/twilio/Twilio.java
@@ -19,7 +19,7 @@ import java.util.concurrent.Executors;
  */
 public class Twilio {
 
-    public static final String VERSION = "7.45.1";
+    public static final String VERSION = "7.46.0";
     public static final String JAVA_VERSION = System.getProperty("java.version");
 
     private static String username;

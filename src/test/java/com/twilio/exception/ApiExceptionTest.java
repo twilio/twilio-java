cff4d88efa922d690df0c444306ebd3d99b0ba02
Ashish Sharma <asharma@twilio.com>
2020-04-22 16:31:25 -0700

    add test for new constructor

--- src/test/java/com/twilio/exception/ApiExceptionTest.java
+++ src/test/java/com/twilio/exception/ApiExceptionTest.java
@@ -1,8 +1,12 @@
 package com.twilio.exception;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
 @SuppressWarnings("ThrowableInstanceNeverThrown")
@@ -13,7 +17,7 @@ public class ApiExceptionTest {
     private final String anyMoreInfo = "more info";
     private final int anyErrorCode = 123;
     private final int anyHttpStatus = 200;
-
+    private final Map<String, Object> details = Collections.singletonMap("foo", new Object());
 
     @Test
     public void singleArgConstructorShouldPreserveMessage() {
@@ -29,13 +33,25 @@ public class ApiExceptionTest {
     }
 
     @Test
-    public void fullConstructorShouldPreserveAllValues() {
+    public void fiveArgConstructorShouldPreserveAllValues() {
         ApiException error = new ApiException(anyMessage, anyErrorCode, anyMoreInfo, anyHttpStatus, anyCause);
         assertEquals("Message", anyMessage, error.getMessage());
         assertSame("Cause", anyCause, error.getCause());
         assertEquals("More info", anyMoreInfo, error.getMoreInfo());
         assertEquals("Error code", anyErrorCode, error.getCode().intValue());
         assertEquals("Status code", anyHttpStatus, error.getStatusCode().intValue());
+        assertNull("Details", error.getDetails());
+    }
+
+    @Test
+    public void fullConstructorShouldPreserveAllValues() {
+        ApiException error = new ApiException(anyMessage, anyErrorCode, anyMoreInfo, anyHttpStatus, details, anyCause);
+        assertEquals("Message", anyMessage, error.getMessage());
+        assertSame("Cause", anyCause, error.getCause());
+        assertEquals("More info", anyMoreInfo, error.getMoreInfo());
+        assertEquals("Error code", anyErrorCode, error.getCode().intValue());
+        assertEquals("Status code", anyHttpStatus, error.getStatusCode().intValue());
+        assertEquals("Details", error.getDetails(), details);
     }
 
     @Test

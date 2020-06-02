7c0d85eacafe634bb864a02a713059a138fb8557
Sam Harrison <sharrison@twilio.com>
2020-06-02 15:40:28 -0500

    feat: add regional and edge support

--- src/main/java/com/twilio/Twilio.java
+++ src/main/java/com/twilio/Twilio.java
@@ -2,7 +2,6 @@ package com.twilio;
 
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
-
 import com.twilio.exception.ApiException;
 import com.twilio.exception.AuthenticationException;
 import com.twilio.exception.CertificateValidationException;
@@ -12,6 +11,7 @@ import com.twilio.http.Request;
 import com.twilio.http.Response;
 import com.twilio.http.TwilioRestClient;
 
+import java.util.Objects;
 import java.util.concurrent.Executors;
 
 /**
@@ -25,6 +25,8 @@ public class Twilio {
     private static String username;
     private static String password;
     private static String accountSid;
+    private static String region;
+    private static String edge;
     private static TwilioRestClient restClient;
     private static ListeningExecutorService executorService;
 
@@ -57,8 +59,8 @@ public class Twilio {
     /**
      * Initialize the Twilio environment.
      *
-     * @param username account to use
-     * @param password auth token for the account
+     * @param username   account to use
+     * @param password   auth token for the account
      * @param accountSid account sid to use
      */
     public static void init(final String username, final String password, final String accountSid) {
@@ -122,9 +124,35 @@ public class Twilio {
     }
 
     /**
+     * Set the region.
+     *
+     * @param region region to make request
+     */
+    public static void setRegion(final String region) {
+        if (!Objects.equals(region, Twilio.region)) {
+            Twilio.invalidate();
+        }
+
+        Twilio.region = region;
+    }
+
+    /**
+     * Set the edge.
+     *
+     * @param edge edge to make request
+     */
+    public static void setEdge(final String edge) {
+        if (!Objects.equals(edge, Twilio.edge)) {
+            Twilio.invalidate();
+        }
+
+        Twilio.edge = edge;
+    }
+
+    /**
      * Returns (and initializes if not initialized) the Twilio Rest Client.
      *
-     * @return the TWilio Rest Client
+     * @return the Twilio Rest Client
      * @throws AuthenticationException if initialization required and either accountSid or authToken is null
      */
     public static TwilioRestClient getRestClient() {
@@ -136,9 +164,14 @@ public class Twilio {
             }
 
             TwilioRestClient.Builder builder = new TwilioRestClient.Builder(Twilio.username, Twilio.password);
+
             if (Twilio.accountSid != null) {
                 builder.accountSid(Twilio.accountSid);
             }
+
+            builder.region(Twilio.region);
+            builder.edge(Twilio.edge);
+
             Twilio.restClient = builder.build();
         }
 

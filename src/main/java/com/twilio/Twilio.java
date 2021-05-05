6a174a0d83526623c36f8874c12bd2d3844df942
Twilio <team_interfaces+github@twilio.com>
2021-05-05 19:14:58 +0000

    Release 8.12.0

--- src/main/java/com/twilio/Twilio.java
+++ src/main/java/com/twilio/Twilio.java
@@ -19,7 +19,7 @@ import java.io.File;
  */
 public class Twilio {
 
-    public static final String VERSION = "8.11.0";
+    public static final String VERSION = "8.12.0";
     public static final String JAVA_VERSION = System.getProperty("java.version");
 
     private static String username = System.getenv("TWILIO_ACCOUNT_SID");

ae895bbc713ea0cc1232ed2bf7b8fd4becfc62cf
Twilio <team_interfaces+github@twilio.com>
2022-06-29 12:19:15 -0700

    Release 8.32.0

--- src/main/java/com/twilio/Twilio.java
+++ src/main/java/com/twilio/Twilio.java
@@ -21,7 +21,7 @@ import java.util.concurrent.Executors;
  */
 public class Twilio {
 
-    public static final String VERSION = "8.31.1";
+    public static final String VERSION = "8.32.0";
     public static final String JAVA_VERSION = System.getProperty("java.version");
     public static final String OS_NAME = System.getProperty("os.name");
     public static final String OS_ARCH = System.getProperty("os.arch");

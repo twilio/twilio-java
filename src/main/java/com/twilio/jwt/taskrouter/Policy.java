49865104e861266937f2db20157e6b724e5b3b87
Priyanka Awatramani <apple@Priyankas-MacBook-Pro.local>
2022-12-11 04:11:10 -0600

    Issue-724 Fixed Flaky Tests

--- src/main/java/com/twilio/jwt/taskrouter/Policy.java
+++ src/main/java/com/twilio/jwt/taskrouter/Policy.java
@@ -1,9 +1,6 @@
 package com.twilio.jwt.taskrouter;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.twilio.http.HttpMethod;
 
@@ -17,6 +14,7 @@ import lombok.ToString;
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonPropertyOrder({"allowed","method","postFilter","queryFilter","url"})
 @ToString
 public class Policy {
 

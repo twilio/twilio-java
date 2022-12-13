53652cfd8f114169b1d16c510775d4221295e658
kridai <kgoswami@twilio.com>
2022-12-14 01:17:34 +0530

    fix: merge conflict

--- src/main/java/com/twilio/rest/messaging/v1/service/UsAppToPerson.java
+++ src/main/java/com/twilio/rest/messaging/v1/service/UsAppToPerson.java
@@ -45,8 +45,8 @@ import java.util.Map;
 public class UsAppToPerson extends Resource {
     private static final long serialVersionUID = 207409378631021L;
 
-    public static UsAppToPersonCreator creator(final String pathMessagingServiceSid, final String brandRegistrationSid, final String description, final String messageFlow, final List<String> messageSamples, final String usAppToPersonUsecase, final Boolean hasEmbeddedLinks, final Boolean hasEmbeddedPhone){
-        return new UsAppToPersonCreator(pathMessagingServiceSid, brandRegistrationSid, description, messageFlow, messageSamples, usAppToPersonUsecase, hasEmbeddedLinks, hasEmbeddedPhone);
+    public static UsAppToPersonCreator creator(final String pathMessagingServiceSid, final String brandRegistrationSid, final String description, final List<String> messageSamples, final String usAppToPersonUsecase, final Boolean hasEmbeddedLinks, final Boolean hasEmbeddedPhone){
+        return new UsAppToPersonCreator(pathMessagingServiceSid, brandRegistrationSid, description, messageSamples, usAppToPersonUsecase, hasEmbeddedLinks, hasEmbeddedPhone);
     }
 
     public static UsAppToPersonDeleter deleter(final String pathMessagingServiceSid, final String pathSid){

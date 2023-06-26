efaeb948b64372433dbb564f27124bdb1008d3a6
sbansla <sbansla@twilio.com>
2023-06-26 22:39:17 +0530

    added enum

---
+++ src/main/java/com/twilio/constant/EnumConstants.java
@@ -0,0 +1,16 @@
+package com.twilio.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+public class EnumConstants {
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum ContentType {
+        JSON("application/json"),
+        FORM_URLENCODED("application/x-www-form-urlencoded");
+
+        private final String value;
+    }
+}

a91fb702a28fd61c6393e01b2aab46becc38bf91
AsabuHere <asabu@twilio.com>
2024-06-13 12:24:15 -0600

    Making token fetch thread safe

--- src/main/java/com/twilio/http/bearertoken/TokenManager.java
+++ src/main/java/com/twilio/http/bearertoken/TokenManager.java
@@ -2,5 +2,5 @@ package com.twilio.http.bearertoken;
 
 public interface TokenManager {
     public String refreshAccessToken(String refreshToken);
-    public String fetchAccessToken();
+    public synchronized String fetchAccessToken();
 }

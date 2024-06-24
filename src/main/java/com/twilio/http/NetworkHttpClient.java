065b5c84169eed7290271d5ed14688e455c6c349
Athira Sabu <102021496+AsabuHere@users.noreply.github.com>
2024-06-24 12:15:31 +0530

    Update NetworkHttpClient.java

--- src/main/java/com/twilio/http/NetworkHttpClient.java
+++ src/main/java/com/twilio/http/NetworkHttpClient.java
@@ -141,6 +141,8 @@ public class NetworkHttpClient extends HttpClient {
         }
 
         if (method != HttpMethod.GET) {
+            // TODO: It will be removed after one RC Release.
+            if (request.getContentType() == null) request.setContentType(EnumConstants.ContentType.FORM_URLENCODED);
             if (EnumConstants.ContentType.JSON.getValue().equals(request.getContentType().getValue())) {
                 HttpEntity entity = new StringEntity(request.getBody(), ContentType.APPLICATION_JSON);
                 builder.setEntity(entity);

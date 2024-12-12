0c4614e82d350bd3d4cc902051f76e31f80632d2
Twilio <team_interfaces+github@twilio.com>
2024-12-12 17:10:31 +0000

    Release 10.6.5

--- src/main/java/com/twilio/Twilio.java
+++ src/main/java/com/twilio/Twilio.java
@@ -24,7 +24,7 @@ import java.util.concurrent.Executors;
  */
 public class Twilio {
 
-    public static final String VERSION = "10.6.4";
+    public static final String VERSION = "10.6.5";
     public static final String JAVA_VERSION = System.getProperty("java.version");
     public static final String OS_NAME = System.getProperty("os.name");
     public static final String OS_ARCH = System.getProperty("os.arch");

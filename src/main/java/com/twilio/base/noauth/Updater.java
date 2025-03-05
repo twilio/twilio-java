b53a830c5c23f3b77223b2cf9a6321e03b359766
sbansla <104902068+sbansla@users.noreply.github.com>
2025-03-05 15:46:33 +0530

    chore: Add domain and noauth base classes (#838)
    * added domain and noauth base files

--- src/main/java/com/twilio/base/noauth/Updater.java
+++ src/main/java/com/twilio/base/noauth/Updater.java
@@ -1,26 +1,25 @@
 package com.twilio.base.noauth;
 
 import com.twilio.Twilio;
-
 import com.twilio.TwilioNoAuth;
 import com.twilio.http.noauth.NoAuthTwilioRestClient;
 
 import java.util.concurrent.CompletableFuture;
 
 /**
- * Executor for fetches of a resource.
+ * Executor for updates of a resource.
  *
  * @param <T> type of the resource
  */
-public abstract class Fetcher<T extends Resource> {
+public abstract class Updater<T extends Resource> {
 
     /**
      * Execute an async request using default client.
      *
      * @return future that resolves to requested object
      */
-    public CompletableFuture<T> fetchAsync() {
-        return fetchAsync(TwilioNoAuth.getRestClient());
+    public CompletableFuture<T> updateAsync() {
+        return updateAsync(TwilioNoAuth.getRestClient());
     }
 
     /**
@@ -29,8 +28,8 @@ public abstract class Fetcher<T extends Resource> {
      * @param client client used to make request
      * @return future that resolves to requested object
      */
-    public CompletableFuture<T> fetchAsync(final NoAuthTwilioRestClient client) {
-        return CompletableFuture.supplyAsync(() -> fetch(client), Twilio.getExecutorService());
+    public CompletableFuture<T> updateAsync(final NoAuthTwilioRestClient client) {
+        return CompletableFuture.supplyAsync(() -> update(client), Twilio.getExecutorService());
     }
 
     /**
@@ -38,8 +37,8 @@ public abstract class Fetcher<T extends Resource> {
      *
      * @return Requested object
      */
-    public T fetch() {
-        return fetch(TwilioNoAuth.getRestClient());
+    public T update() {
+        return update(TwilioNoAuth.getRestClient());
     }
 
     /**
@@ -48,5 +47,5 @@ public abstract class Fetcher<T extends Resource> {
      * @param client client used to make request
      * @return Requested object
      */
-    public abstract T fetch(final NoAuthTwilioRestClient client);
+    public abstract T update(final NoAuthTwilioRestClient client);
 }

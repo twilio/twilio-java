f0aa2cbc1562fdcb028d504667f37be648ba45e5
sbansla <104902068+sbansla@users.noreply.github.com>
2025-09-16 20:59:40 +0530

    fix: Reuse twiliorestclient for orgs api (#896)
    orgs api can be used just like other apis, removed orgs api rest client and related files.

--- src/main/java/com/twilio/rest/previewiam/organizations/RoleAssignmentDeleter.java
+++ src/main/java/com/twilio/rest/previewiam/organizations/RoleAssignmentDeleter.java
@@ -14,15 +14,15 @@
 
 package com.twilio.rest.previewiam.organizations;
 
-import com.twilio.base.bearertoken.Deleter;
+import com.twilio.base.Deleter;
 import com.twilio.constant.EnumConstants;
 import com.twilio.exception.ApiConnectionException;
 import com.twilio.exception.ApiException;
 import com.twilio.exception.RestException;
 import com.twilio.http.HttpMethod;
 import com.twilio.http.Response;
-import com.twilio.http.bearertoken.BearerTokenRequest;
-import com.twilio.http.bearertoken.BearerTokenTwilioRestClient;
+import com.twilio.http.Request;
+import com.twilio.http.TwilioRestClient;
 import com.twilio.rest.Domains;
 
 public class RoleAssignmentDeleter extends Deleter<RoleAssignment> {
@@ -39,7 +39,7 @@ public class RoleAssignmentDeleter extends Deleter<RoleAssignment> {
     }
 
     @Override
-    public boolean delete(final BearerTokenTwilioRestClient client) {
+    public boolean delete(final TwilioRestClient client) {
         String path =
             "/Organizations/{OrganizationSid}/RoleAssignments/{RoleAssignmentSid}";
 
@@ -54,7 +54,7 @@ public class RoleAssignmentDeleter extends Deleter<RoleAssignment> {
                 this.pathRoleAssignmentSid.toString()
             );
 
-        BearerTokenRequest request = new BearerTokenRequest(
+        Request request = new Request(
             HttpMethod.DELETE,
             Domains.PREVIEWIAM.toString(),
             path
@@ -67,7 +67,7 @@ public class RoleAssignmentDeleter extends Deleter<RoleAssignment> {
                 "RoleAssignment delete failed: Unable to connect to server"
             );
         } else if (
-            !BearerTokenTwilioRestClient.SUCCESS.test(response.getStatusCode())
+            !TwilioRestClient.SUCCESS.test(response.getStatusCode())
         ) {
             RestException restException = RestException.fromJson(
                 response.getStream(),

6f86644230a59405b0f90daf940b8003949b17f0
Twilio <team_interfaces+github@twilio.com>
2026-02-18 04:21:03 +0000

    Release 11.3.4

--- src/main/java/com/twilio/Twilio.java
+++ src/main/java/com/twilio/Twilio.java
@@ -41,7 +41,7 @@ import org.slf4j.LoggerFactory;
  */
 public class Twilio {
 
-    public static final String VERSION = "11.3.3";
+    public static final String VERSION = "11.3.4";
     public static final String JAVA_VERSION = System.getProperty("java.version");
     public static final String OS_NAME = System.getProperty("os.name");
     public static final String OS_ARCH = System.getProperty("os.arch");
